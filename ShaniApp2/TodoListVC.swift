e405c8e3eae4c738d6fed74d2ddb5153afc83780
Shani Zlotnik <shani.zlotnik@autodesk.com>
2018-09-16 16:43:05 +0300

    post still works, networking moved to new file (only post and get functions only) and it works

--- ShaniApp2/TodoListVC.swift
+++ ShaniApp2/TodoListVC.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class TodoListVC: UITableViewController, AddTaskDelegate,  ChangeButton {
     
+    let networking = Networking()
     var tasksTodo: [TaskTodo] = []
-    var tempID = 1
-    
-    let urlGet = "http://ec2-52-32-105-2.us-west-2.compute.amazonaws.com:8080/all"
-    let urlPost = "http://ec2-52-32-105-2.us-west-2.compute.amazonaws.com:8080/new"
-    let urlPut = "http://ec2-52-32-105-2.us-west-2.compute.amazonaws.com:8080/update/{item_id}"
-    let urlDelete = "http://ec2-52-32-105-2.us-west-2.compute.amazonaws.com:8080/delete/{item_id}"
+//    var tempID = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getJsonFromUrl(completion: { [weak self] in
+        networking.getJsonFromUrl(completion: { [weak self] in
+            self?.tasksTodo = (self?.networking.tasksTodoArray)!
             self?.tableView.reloadData()
             print("test: reloaded view")
         })
-        
         print(tasksTodo)
     }
     
@@ -60,60 +56,68 @@ class TodoListVC: UITableViewController, AddTaskDelegate,  ChangeButton {
             self.tableView.reloadData()
         }
     }
-    
-    func getJsonFromUrl(completion: @escaping () -> Void) {
-        
-        guard let url = URL(string: urlGet) else { return }
-        
-        let session = URLSession.shared
-        session.dataTask(with: url) { [weak self] (data, response, error) in
-            guard let jsonData = data else { return }
-            
-            do {
-                self?.tasksTodo = try JSONDecoder().decode([TaskTodo].self, from: jsonData)
-                print(self?.tasksTodo ?? "")
-            } catch {
-                print(error.localizedDescription)
-            }
-            DispatchQueue.main.async {
-                completion()
-            }
-        }.resume()
+
+    func taskAdded(name: String) {
+        networking.taskAddedPOST(name: name) { [weak self] in
+            self?.tasksTodo = (self?.networking.tasksTodoArray)!
+            self?.reloadList()
+            self?.navigationController?.popViewController(animated: true)
+            print("test: reloaded view")
+        }
     }
+    
+//    func getJsonFromUrl(completion: @escaping () -> Void) {
+//
+//        guard let url = URL(string: urlGet) else { return }
+//
+//        let session = URLSession.shared
+//        session.dataTask(with: url) { [weak self] (data, response, error) in
+//            guard let jsonData = data else { return }
+//
+//            do {
+//                self?.tasksTodo = try JSONDecoder().decode([TaskTodo].self, from: jsonData)
+//                print(self?.tasksTodo ?? "")
+//            } catch {
+//                print(error.localizedDescription)
+//            }
+//            DispatchQueue.main.async {
+//                completion()
+//            }
+//        }.resume()
+//    }*
     
     
 
-    func taskAdded(name:String) {
-        let parametersJson: [String : Any] = ["id":tempID, "title":name, "completed":false]
-        let jsonDataToPost = try? JSONSerialization.data(withJSONObject: parametersJson)
-        
-        guard let url = URL(string: urlPost) else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = jsonDataToPost
-        
-        let sessionTask = URLSession.shared
-        sessionTask.dataTask(with: request) { [weak self] (data, response, error) in
-            if let response = response {
-                print(response)
-            }
-            
-            if let data = data {
-                print(data)
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print(json)
-                } catch {
-                    print(error)
-                }
-            DispatchQueue.main.async {
-                self?.viewDidLoad()
-                //self?.tableView.reloadData()
-                self?.navigationController?.popViewController(animated: true)
-            }
-        }
-    }.resume()
-}
+//    func taskAdded(name:String) {
+//        let parametersJson: [String : Any] = ["id":tempID, "title":name, "completed":false]
+//        let jsonDataToPost = try? JSONSerialization.data(withJSONObject: parametersJson)
+//
+//        guard let url = URL(string: networking.urlPost) else { return }
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        request.httpBody = jsonDataToPost
+//
+//        let sessionTask = URLSession.shared
+//        sessionTask.dataTask(with: request) { [weak self] (data, response, error) in
+//            if let response = response {
+//                print(response)
+//            }
+//
+//            if let data = data {
+//                print(data)
+//                do {
+//                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+//                    print(json)
+//                } catch {
+//                    print(error)
+//                }
+//            DispatchQueue.main.async {
+//                self?.viewDidLoad()
+//                self?.navigationController?.popViewController(animated: true)
+//            }
+//        }
+//    }.resume()
+//}*
     
     func changeButton(checked: Bool, index: Int?) {
         tasksTodo[index!].completed = checked
